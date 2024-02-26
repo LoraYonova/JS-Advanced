2dd23591fbb63de571e99de3752038f2ce213ba4
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-02-26 15:01:42 +0200

    Task 9

---
+++ Syntax, Functions and Statements/squareOfStars.js
@@ -0,0 +1,10 @@
+function printRectangle (param = 5) {
+
+    for (let i = 1; i <= param; i++){
+        console.log('*'.repeat(param));
+    }
+
+}
+printRectangle(10);
+printRectangle(2);
+printRectangle();
