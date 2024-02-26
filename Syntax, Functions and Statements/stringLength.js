5e23a42f241802e8a557c793e94ae18a72aef0df
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-02-26 13:48:07 +0200

    Task 2

---
+++ Syntax, Functions and Statements/stringLength.js
@@ -0,0 +1,11 @@
+function solve(arr1, arr2, arr3) {
+    let sum = arr1.length + arr2.length + arr3.length;
+    let avg = Math.floor(sum / 3);
+
+    console.log(sum);
+    console.log(avg);
+
+}
+
+solve('chocolate', 'ice cream', 'cake');
+solve('pasta', '5', '22.3');
