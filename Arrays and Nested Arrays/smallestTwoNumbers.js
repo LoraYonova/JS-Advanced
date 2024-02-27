746fe2e56e728d7e38e7b35733276e064025f81e
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-02-27 15:00:13 +0200

    Smallest Two Numbers

---
+++ Arrays and Nested Arrays/smallestTwoNumbers.js
@@ -0,0 +1,10 @@
+function printTwoSmallestNumbers(arr) {
+    
+    arr.sort((a, b) => a - b);
+    
+    console.log(arr[0], arr[1]);
+}
+
+
+printTwoSmallestNumbers([30, 15, 50, 5]); 
+printTwoSmallestNumbers([3, 0, 10, 4, 7, 3]);
