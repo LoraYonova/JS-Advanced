9c997fb86c055b356b5ab8888bbce3e6cfd522e2
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-05 13:05:34 +0200

    Edit Element

--- DOM Introduction/01.Edit Element/EditElement.js
+++ DOM Introduction/01.Edit Element/EditElement.js
@@ -1,3 +1,8 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+    
+    let content = element.textContent;
+    let matcher = new RegExp(match, 'g');
+    let edited = content.replace(matcher, replacer);
+   
+    element.textContent = edited;
 }
