20dded7cbb373b533f296914a3853063c28bdd87
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-05 14:43:59 +0200

    Sum Table

--- DOM Introduction/06.Sum Table/SumTable.js
+++ DOM Introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,19 @@
 function sumTable() {
 
+   let allElements = document.querySelectorAll('tbody tr');
+
+   let sum = 0;
+
+   for (let i = 1; i < allElements.length - 1; i++) {
+   
+        let children = allElements[i].children;
+        let cost = children[children.length -1].textContent;
+        sum += Number(cost);
+
+   }
+
+   console.log(sum);
+
+   document.getElementById('sum').textContent = sum;
+
 }
