9830b598404c5d8881e4cdc0c19fc703f51ff731
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-05 15:00:54 +0200

    Extract Parenthesis

--- DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,17 @@
 function extract(content) {
 
+    let targetElement = document.getElementById(content);
+    const textContent = targetElement.textContent;
+    const regex = /\((.*?)\)/g;
+    const matches = textContent.match(regex);
+
+    if (!matches) {
+        return "";
+    }
+
+    let extractedText = matches.map(match => match.slice(1, -1)).join("; ");
+
+   return document.getElementById('holder').textContent = extractedText;
+
 }
+
