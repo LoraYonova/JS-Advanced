84ce216dd93d94269c3eb5b77a60c7cec4567100
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-07 17:48:13 +0200

    Pascal or Camel Case

--- DOM Introduction Exercise/02. Pascal or Camel Case/solution.js
+++ DOM Introduction Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO...
+  
+   let text = document.getElementById("text").value.toLowerCase();
+   let namingConvention = document.getElementById("naming-convention").value;
+
+   let arrText = text.split(" ");
+   let result = "";
+
+   if (namingConvention === "Camel Case") {
+   result = arrText.shift();
+   arrText.forEach(element => {
+
+    result += element[0].toUpperCase() + element.substring(1).toLowerCase();
+    
+   });
+    
+  } else if (namingConvention === "Pascal Case") {
+    arrText.forEach(element => {
+      result += element[0].toUpperCase() + element.substring(1).toLowerCase();
+    })
+
+  } else {
+    result = "Error!";
+}
+  
+   document.getElementById("result").textContent = result;
+ 
 }
