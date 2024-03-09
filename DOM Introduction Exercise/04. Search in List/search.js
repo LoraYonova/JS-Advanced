4a1adb65fdd2fc24c4d48b8894e7bf683a5a05d7
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-09 13:37:58 +0200

    Search in List

--- DOM Introduction Exercise/04. Search in List/search.js
+++ DOM Introduction Exercise/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+   
+   let towns = document.getElementById("towns").getElementsByTagName("li");
+  
+   let search = document.getElementById("searchText").value.toLowerCase().trim();
+
+   let result = 0;
+   for (let i = 0; i < towns.length; i++) {
+      let townText = towns[i].textContent.toLowerCase().trim();
+
+      if (townText.includes(search)) {
+          result++;
+          towns[i].style.fontWeight = "bold";
+      } else {
+          towns[i].style.fontWeight = "normal"; 
+          towns[i].style.textDecoration = "none"; 
+      }
+  }
+
+   
+
+   let text = document.getElementById("result");
+
+   text.textContent = (`${result} matches found`)
 }
