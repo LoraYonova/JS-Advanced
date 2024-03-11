f05b0cdac8b619241c9efb7768fc155c6db74c7c
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-11 10:35:21 +0200

    Table - Search Engin

--- DOM Introduction Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let search = document.getElementById("searchField").value;
+      
+    let table = document.getElementsByTagName("tr");
+   
+    for (let i = 2; i < table.length; i++) {
+         let row = table[i].textContent.toLowerCase();
+        
+      if (row.includes(search.toLowerCase())) {
+         table[i].style.backgroundColor = "yellow";
+       }
 
+    }
    }
 }
