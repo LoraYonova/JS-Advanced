e691b1e49f6c1ae4771f8061bac573d1209d0388
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-12 18:51:24 +0200

    Format the Text

--- DOM Introduction Exercise/06. Format the Text/solution.js
+++ DOM Introduction Exercise/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  
+  let inputText = document.getElementById("input").value.trim();
+  let sentences = inputText.split('.'); 
+
+  let outputContainer = document.getElementById("output");
+  outputContainer.innerHTML = '';
+
+  let paragraphSentences = [];
+  for (let i = 0; i < sentences.length; i++) {
+      paragraphSentences.push(sentences[i]); 
+
+   
+      if (paragraphSentences.length === 3 || i === sentences.length - 1) {
+          let paragraph = document.createElement("p");
+          paragraph.textContent = paragraphSentences.join('. '); 
+          outputContainer.appendChild(paragraph);
+          paragraphSentences = []; 
+      }
+  }
 }
