12dbfdf61d57e31479d010632fcd49d1fe160a0c
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-13 11:06:54 +0200

    Add Delete

--- DOM Events Lab/03. Add-Delete/app.js
+++ DOM Events Lab/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+
+    let input = document.getElementById("newItemText").value;
+    if (input.length === 0) return;
+
+    let items = document.getElementById("items");
+
+    let listElement = document.createElement("li");
+    listElement.textContent = input;
+
+    let remove = document.createElement("a");
+    remove.textContent = "[Delete]";
+    remove.href = "#";
+    remove.onclick = () => listElement.remove();
+
+    listElement.appendChild(remove);
+    items.appendChild(listElement);
 }
+
