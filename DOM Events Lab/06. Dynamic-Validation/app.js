f6016c2e48edc0d84def5f2a100aa7d6a5af95df
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-13 11:34:29 +0200

    Dynamic-Validation

--- DOM Events Lab/06. Dynamic-Validation/app.js
+++ DOM Events Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+    let emailInput = document.getElementById("email");
+
+    emailInput.addEventListener("change", function() {
+        let email = emailInput.value.trim();
+        let isValid = /^[a-z]+@[a-z]+\.[a-z]+$/.test(email);
+
+        if (!isValid) {
+            emailInput.classList.add("error");
+        } else {
+            emailInput.classList.remove("error");
+        }
+    });
 }
+
