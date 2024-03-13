6a48a3cecd5638a7412322676201c24c2ac86a19
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-13 12:25:00 +0200

    Shopping Cart

--- DOM Events Lab/07. Shopping-Cart/solution.js
+++ DOM Events Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-   //TODO...
+ 
+   let checkoutButton = document.querySelector(".checkout");
+   let addButtons = document.querySelectorAll(".add-product");
+   let textarea = document.querySelector("textarea");
+   let totalPrice = 0;
+   let products = new Set();
+
+   addButtons.forEach(button => {
+       button.addEventListener("click", function() {
+           let product = button.parentNode.parentNode;
+           let productName = product.querySelector(".product-title").textContent;
+           let productPrice = Number(product.querySelector(".product-line-price").textContent);
+
+           totalPrice += productPrice;
+           products.add(productName);
+
+           textarea.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
+       });
+   });
+
+   checkoutButton.addEventListener("click", function() {
+       let productList = [...products].join(", ");
+       textarea.value += `You bought ${productList} for ${totalPrice.toFixed(2)}.`;
+
+       addButtons.forEach(button => {
+           button.disabled = true;
+       });
+       checkoutButton.disabled = true;
+   });
 }
